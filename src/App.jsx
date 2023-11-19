45d20be97029237a1c5dbb9383fabb28f405b08e
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-11-19 13:45:13 -0500

    add existing project files to git

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// src/App.js
+
+import React from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+    </main>
+  );
+}
